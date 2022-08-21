8c1bdb72e8e077091dd9794939518ae75d5556b2
Irek <general157@mail.ru>
2022-08-21 15:23:50 +0300

    add Example8

---
+++ Example8/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число больше 1: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+if (N <= 1)
+{
+    Console.WriteLine("Нужно ввести число больше 1!");
+}
+
+else
+{
+    while (i < (N - 1))
+    {
+        Console.Write(i + ", ");
+        i = i + 2;
+    }
+    Console.Write(i);
+}
